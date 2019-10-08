2c404a98016a167f55c58c5770e2ae19bcf2a51d
vai na web <vainawebcpx@gmail.com>
2019-10-08 17:49:26 -0300

    Projeto todo

---
+++ main.js
@@ -0,0 +1,14 @@
+let imagem = document.querySelector('#imagem');
+let cont = 0;
+
+let imagens = [
+  "./1.png",
+  "./2.png",
+]
+
+imagem.addEventListener('click', () => {
+  imagem.setAttribute("src", "./2.png");
+
+  
+});
+
